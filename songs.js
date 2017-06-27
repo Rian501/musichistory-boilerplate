7177714c73a1184c68c074d8f3e5e9801991b5ca
Rian501 <eclaire.lemmon@gmail.com>
2017-06-27 11:57:46 -0500

    Make add-view and set to ishidden

--- songs.js
+++ songs.js
@@ -7,31 +7,19 @@ songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album App
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 
-// console.log("Is this the whole array?", songs);
-
 //Add one song to the beginning and the end of the array.
 songs.unshift("The Way You Make Me Feel > by Michael Jackson on the album Bad")
 songs.push("24 Frames > by Jason Isbell and the 400 Unit on the album Something More Than Free")
 
-// console.log("Did I add stuff to the array?", songs);
-
-// Loop over the array, and remove any words or characters that obviously don't belong.
 for (i=0; i<songs.length; i++) {
 	songs[i]=songs[i].replace(/@/g,"")
 	songs[i]=songs[i].replace(/\*/g,"")
 	songs[i]=songs[i].replace(/\(/g,"")
 	songs[i]=songs[i].replace(/!/g,"")
-	// Find and replace the > character in each item with a - character.
 	songs[i]=songs[i].replace(/>/g,"-")
 }
 
-// console.log("Are the arrays cleaned up of ridic chars?", songs);
 
-
-// Add each string to the DOM in index.html in the main content area.
-// Example output:
-
-// {Song name} by {Artist} on the album {Album}
 
 let songDisplayDiv = document.getElementById('songDisplay')
 
@@ -48,4 +36,16 @@ for (i=0; i < songs.length; i++) {
 	}
 	
 
-songDisplayDiv.innerHTML = songsContentDisplay;		
+songDisplayDiv.innerHTML = songsContentDisplay;
+
+
+let displayAddViewLink = document.getElementById('showAddviewLink');
+
+displayAddViewLink.addEventListener("click", function() {
+	//toggle ishidden
+	console.log("I cliked it", );
+	let spaTypes = document.getElementsByClassName('pagecontainer')
+	spaTypes.forEach(function(pagecontainer) {
+		pagecontainer.classList.toggle('ishidden');
+	});
+});
