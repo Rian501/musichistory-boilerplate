88e0950f9ac5b9721894505924ef3c6d0bbccb9f
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-07-02 14:33:38 -0500

    The JSON data prints correctly to DOM

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -1,42 +1,48 @@
 // need to print to DOM the preloaded songs and also
 //take in input, push to array for cleaning, and then print those to DOM also
+var SongDisplay = (function (globalScopeSD) {
+	let DOMint = Object.create(null);
+
+	let addMusicBtn = document.getElementById('addMusicBtn')
+
+	addMusicBtn.addEventListener('click', function() {
+		addUserSongToArray();
+		toggleIsHidden();
+	})
+
+	DOMint.addUserSongToArray = function() {
+		let getSongTitleUserInput = document.getElementById('song-input').value;
+		let getSongArtistUserInput = document.getElementById('artist-input').value;
+		let getSongAlbumUserInput = document.getElementById('album-input').value;
+		let songToAdd = `${getSongTitleUserInput} > by ${getSongArtistUserInput} on the album ${getSongAlbumUserInput}`
+		console.log("song to add?", songToAdd);
+		addNewSong(cleanUpSongs, songs, songToAdd);
+	}
 
 
-let addMusicBtn = document.getElementById('addMusicBtn')
+	DOMint.printSongsToDOM = function(songsArray) {
+		let songsContentDisplay = '';
+		let songDisplayDiv = document.getElementById('songDisplay')
+		for (i=0; i < songsArray.length; i++) {
 
-addMusicBtn.addEventListener('click', function() {
-	addUserSongToArray();
-	toggleIsHidden();
-})
-
-function addUserSongToArray() {
-	let getSongTitleUserInput = document.getElementById('song-input').value;
-	let getSongArtistUserInput = document.getElementById('artist-input').value;
-	let getSongAlbumUserInput = document.getElementById('album-input').value;
-	let songToAdd = `${getSongTitleUserInput} > by ${getSongArtistUserInput} on the album ${getSongAlbumUserInput}`
-	console.log("song to add?", songToAdd);
-	addNewSong(cleanUpSongs, songs, songToAdd);
-}
-
-
-function printSongsToDOM(songsArray) {
-	let songsContentDisplay = '';
-	let songDisplayDiv = document.getElementById('songDisplay')
-	for (i=0; i < songs.length; i++) {
-
-		songsContentDisplay += `
-			<section class="dispSong" id="${songs[i]}">
-			<h3>${songs[i]}</h3>
-			</section>
-			`
-		}
-	songDisplayDiv.innerHTML = songsContentDisplay;
-}
+			songsContentDisplay += `
+				<section class="dispSong" id="${songsArray[i]}">
+				<h3>${songsArray[i].title}</h3><h4>by ${songsArray[i].artist} on the album ${songsArray[i].album}
+				</section>
+				`
+			}
+			
+		songDisplayDiv.innerHTML = songsContentDisplay;
+	}
 
 
 
-function addNewSong(nextStep, songArray, newSong) {
-	songArray.unshift(newSong);
-	nextStep(songArray);
-}
+	function addNewSong(nextStep, songArray, newSong) {
+		songArray.unshift(newSong);
+		nextStep(songArray);
+	}
 
+	globalScopeSD.DOMint = DOMint;
+	return globalScopeSD;
+
+})(SongDisplay || {});
--- javascripts/processData.js
+++ javascripts/processData.js
@@ -1,14 +1,29 @@
 //need to deal with array of objects from JSON, and also clean up of unwanted chars
 
+//songsArray is an array of objects
 
+
+//this seems really repeptetive and there is probably a cleaner way to do this.
 function cleanUpSongs(songsArray) {
 	for (i=0; i<songsArray.length; i++) {
-		songsArray[i]=songsArray[i].replace(/@/g,"")
-		songsArray[i]=songsArray[i].replace(/\*/g,"")
-		songsArray[i]=songsArray[i].replace(/\(/g,"")
-		songsArray[i]=songsArray[i].replace(/!/g,"")
-		songsArray[i]=songsArray[i].replace(/>/g,"-")
+		songsArray[i].title=songsArray[i].title.replace(/@/g,"")
+		songsArray[i].title=songsArray[i].title.replace(/\*/g,"")
+		songsArray[i].title=songsArray[i].title.replace(/\(/g,"")
+		songsArray[i].title=songsArray[i].title.replace(/!/g,"")
+		songsArray[i].title=songsArray[i].title.replace(/>/g,"-")
+
+		songsArray[i].album=songsArray[i].album.replace(/@/g,"")
+		songsArray[i].album=songsArray[i].album.replace(/\*/g,"")
+		songsArray[i].album=songsArray[i].album.replace(/\(/g,"")
+		songsArray[i].album=songsArray[i].album.replace(/!/g,"")
+		songsArray[i].album=songsArray[i].album.replace(/>/g,"-")
+
+		songsArray[i].artist=songsArray[i].artist.replace(/@/g,"")
+		songsArray[i].artist=songsArray[i].artist.replace(/\*/g,"")
+		songsArray[i].artist=songsArray[i].artist.replace(/\(/g,"")
+		songsArray[i].artist=songsArray[i].artist.replace(/!/g,"")
+		songsArray[i].artist=songsArray[i].artist.replace(/>/g,"-")
 	}
 	console.log("song data clean?", songsArray);
-	//printSongsToDOM(songs);
+	SongDisplay.DOMint.printSongsToDOM(songsArray);
 }
