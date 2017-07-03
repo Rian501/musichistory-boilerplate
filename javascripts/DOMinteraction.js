e487867721c99b5ecd08e859ed747771d0588388
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-07-02 20:39:13 -0500

    Add delete button and have it remove song from DOM

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -1,8 +1,8 @@
-// need to print to DOM the preloaded songs and also
-//take in input, push to array for cleaning, and then print those to DOM also
 var SongDisplay = (function (globalScopeSD) {
 	let DOMint = Object.create(null);
-
+	let songsContentDisplay = '';
+	let songDisplayDiv = document.getElementById('songDisplay')
+	let userAddedSongs = [];
 	let addMusicBtn = document.getElementById('addMusicBtn')
 
 	addMusicBtn.addEventListener('click', function() {
@@ -11,14 +11,10 @@ var SongDisplay = (function (globalScopeSD) {
 	})
 
 	DOMint.addUserSongToArray = function() {
-		let userAddedSongs = [];
-		let getSongTitleUserInput = document.getElementById('song-input').value;
-		let getSongArtistUserInput = document.getElementById('artist-input').value;
-		let getSongAlbumUserInput = document.getElementById('album-input').value;
 		let songToAdd = {};
-			songToAdd.title = getSongTitleUserInput;
-			songToAdd.artist = getSongArtistUserInput;
-			songToAdd.album = getSongAlbumUserInput;
+			songToAdd.title = document.getElementById('song-input').value;
+			songToAdd.artist = document.getElementById('artist-input').value;
+			songToAdd.album = document.getElementById('album-input').value;
 	
 		console.log("song to add?", songToAdd);
 		addNewSong(cleanUpSongs, songsArray, songToAdd);
@@ -26,9 +22,6 @@ var SongDisplay = (function (globalScopeSD) {
 		console.log("user added songs array?", userAddedSongs);
 	};
 
-	let songsContentDisplay = '';
-	let songDisplayDiv = document.getElementById('songDisplay')
-	let userAddedSongs = [];
 	DOMint.printSongsToDOM = function(songsArray) {
 		console.log("songs Array?", songsArray);
 		songsArray = songsArray.concat(userAddedSongs);
@@ -36,22 +29,27 @@ var SongDisplay = (function (globalScopeSD) {
 
 			songsContentDisplay += `
 				<section class="dispSong" id="${songsArray[i]}">
-				<h3>${songsArray[i].title}</h3><h4>by ${songsArray[i].artist} on the album ${songsArray[i].album}
+					<h3>${songsArray[i].title}</h3><h4>by ${songsArray[i].artist} on the album ${songsArray[i].album}
+					<button class="delete">delete song</button>
 				</section>
 				`
 			}
-
 		songDisplayDiv.innerHTML = songsContentDisplay;
-		//return songsArray
 	}
 
-
+	window.addEventListener("click", function() {
+		if (event.target.classList.contains('delete')) {
+			event.target.parentNode.parentNode.remove();
+		};
+	})
 
 	function addNewSong(nextStep, songArray, newSong) {
-		console.log("song array?", songArray);
 		songArray.unshift(newSong);
-		console.log("song array?", songArray);
 		nextStep(songArray);
+	}
+
+	function deleteSong (event) {
+
 	}
 
 	globalScopeSD.DOMint = DOMint;
